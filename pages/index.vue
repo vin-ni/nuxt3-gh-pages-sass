40728b1761921529deee3a823f79de936e7c7339
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-03-15 00:11:19 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <h1>Title 1</h1>
+    <h1>Title</h1>
     <p>Loorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
       magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
       gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
