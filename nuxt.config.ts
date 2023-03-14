f0c3d47763b14ecbcef504528aa100f95a1c6ade
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-03-15 00:08:29 +0100

    without utl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL: '/nuxt3-gh-pages-sass/'
+        baseURL: '/'
     },
     // target: 'static',
     css: ["assets/scss/main.scss"],
