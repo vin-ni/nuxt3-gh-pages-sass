7b79f5038e0ecdf2e89acdba3fdcb45daa34012b
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-03-14 23:46:15 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    app: {
+        baseURL: '/nuxt3-gh-pages-sass/'
+    },
+    // target: 'static',
+    css: ["assets/scss/main.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/assets/scss/mixins.scss";',
+                },
+            },
+        },
+    },
+})
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <h1>Title</h1>
+    <p>Test Text</p>
+    <p>Loorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet.</p>
+
+    <img src="/image.png" alt="">
+
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet.</p>
+
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet.</p>
+  </div>
+</template>
+
+
+<style lang="scss">
+@import "@/assets/scss/mixins.scss";
+
+.wrapper {
+  width: 100%;
+  padding: 24px;
+  max-width: 900px;
+  margin: 0 auto;
+
+  h1 {
+    font-size: 48px;
+    margin-bottom: 24px;
+
+    @include onScreen('phone-only') {
+      font-size: 32px;
+    }
+  }
+
+  p {
+    font-size: 24px;
+    @include onScreen('phone-only') {
+      font-size: 16px;
+    }
+  }
+
+  img {
+    margin-bottom: 24px;
+  }
+
+  background-color: yellow;
+  @include onScreen('phone-only') {
+    background-color: greenyellow;
+  }
+}
+</style>
