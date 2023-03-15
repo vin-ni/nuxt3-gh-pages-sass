8c861911890ce02406ecab8dfbb3890bcbe2d559
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-03-15 01:30:40 +0100

    testing nuxt link

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -10,14 +10,14 @@ const { data: news } = await useAsyncData('news', () => {
       <div v-if="news">
         <div class="news-archive-container">
           <div v-for="article in news">
-            <a :href="article._path" class="container">
+            <NuxtLink :to="article._path" class="container">
               <div class="img-container">
                 <img :src="article.teaser_img.src" :alt="article.teaser_img.alt">
               </div>
               <h4>
                 {{ article.title }}
               </h4>
-            </a>
+            </NuxtLink>
 
 
           </div>
